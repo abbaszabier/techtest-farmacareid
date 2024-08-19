c6b5bdf4d1426ab0e835659b21866e12479ab729
abbaszabier <abbas.zabier06@gmail.com>
2024-08-19 09:30:54 +0700

    update route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/Home";
+import DetailPokemon from "../pages/Home/detailPokemon";
+import ConfirmationUpdatesStock from "../pages/Home/confirmationUpdateStock";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/pokemon/:name",
+        element: <DetailPokemon />,
+      },
+      {
+        path: "/pokemon/:name/konfirmasi-update-stok",
+        element: <ConfirmationUpdatesStock />,
+      },
+    ],
   },
 ]);
 
