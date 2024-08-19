e9a2e12e6608e904e3bc65f9fc4fadf63cc8fd7e
abbaszabier <abbas.zabier06@gmail.com>
2024-08-19 09:29:57 +0700

    create formatter

---
+++ src/utils/formatter.tsx
@@ -0,0 +1,11 @@
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  return new Intl.DateTimeFormat("en-GB", options).format(date);
+};
